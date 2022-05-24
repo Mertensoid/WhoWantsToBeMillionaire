3ceb35bc20a183c778a95a2c6a0eac8fa279d8c6
Mertensoid <matsoukm@yandex.ru>
2022-05-24 19:34:56 +0300

    Initial Commit

---
+++ WhoWantsToBeMillionaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoWantsToBeMillionaire
+//
+//  Created by admin on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
